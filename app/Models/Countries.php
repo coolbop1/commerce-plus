c742918df815d6e0bf1700f38adadc0bab6e2fff
MAC <mac@MACs-MacBook-Pro.local>
2023-02-05 16:03:03 +0100

    Add store controller

---
+++ app/Models/Countries.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Countries extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'code'
+    ];
+
+    public function states() {
+      return  $this->hasMany(States::class, 'country_id');
+    }
+} 
+
