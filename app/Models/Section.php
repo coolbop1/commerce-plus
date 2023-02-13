b0532518b4f90d1913fc7b652e02086ce581fb48
MAC <mac@MACs-MacBook-Pro.local>
2023-02-14 00:57:40 +0100

    seed-subcategorie

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -6,25 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Section extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = [
-        'name', 'description', 'verified',  'user_id'
+        'name', 'description', 'verified',  'user_id', 'sub_category_id'
     ];
 
     protected $casts = [
         'verified' => 'boolean',
     ];
 
-    public function subCategories()
+    public function subCategory()
     {
-        return $this->hasMany(SubCategory::class);
+        return $this->belongsTo(SubCategory::class);
     }
 
     public function products()
     {
         return $this->hasMany(Product::class);
     }
-
 }
---
+++ database/migrations/2023_02_13_190811_add_sub_category_id_to_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('sub_category_id')->after('category_id')->nullable();
+            $table->integer('section_id')->after('category_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
