b5259b06ad0e02c28ab3accddf2b17c425adc4bc
MAC <mac@MACs-MacBook-Pro.local>
2023-04-10 21:46:37 +0100

    clean-up-fe

--- resources/views/layouts/todays-deal.blade.php
+++ resources/views/layouts/todays-deal.blade.php
@@ -9,142 +9,40 @@
         </div>
         <div class="c-scrollbar-light overflow-auto h-lg-400px p-2 bg-primary rounded-bottom">
             <div class="gutters-5 lg-no-gutters row row-cols-2 row-cols-lg-1">
-                                                                            <div class="col mb-2">
-                    <a href="https://demo.activeitzone.com/ecommerce/product/Philips-Rice-Cooker-06L-4BWjg" class="d-block p-2 text-reset bg-white h-100 rounded">
-                        <div class="row gutters-5 align-items-center">
-                            <div class="col-xxl">
-                                <div class="img">
-                                    <img
-                                        class="lazyload img-fit h-140px h-lg-80px"
-                                        src="https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg"
-                                        data-src="https://demo.activeitzone.com/ecommerce/public/uploads/all/5DpZmEosn1JKGBEv7LnmKaY2z3oN9TIlj6zSgSCs.jpg"
-                                        alt="Philips Rice Cooker 0.6L"
-                                        onerror="this.onerror=null;this.src='https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg';"
-                                    >
+                @foreach ($productsByStore->take(20) as $store)
+                    @php
+                        $product = optional($store->values())->get(3) ?? null;
+                        if(is_null($product)){
+                            continue;
+                        }
+                    @endphp
+                        <div class="col mb-2">
+                            <a href="/product/{{ $product->slug }}" class="d-block p-2 text-reset bg-white h-100 rounded">
+                                <div class="row gutters-5 align-items-center">
+                                    <div class="col-xxl">
+                                        <div class="img">
+                                            <img
+                                                class="lazyload img-fit h-140px h-lg-80px"
+                                                src="/assets/img/placeholder.jpg"
+                                                data-src="/{{ $product->thumbnail_img }}"
+                                                alt="{{ $product->name }}"
+                                                onerror="this.onerror=null;this.src='/assets/img/placeholder.jpg';"
+                                            >
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl">
+                                        <div class="fs-16">
+                                            <span class="d-block text-primary fw-600">₦{{ number_format($product->newprice, 2) }}</span>
+                                            @if ($product->hasDiscount())
+                                            <del class="d-block opacity-70">₦{{ number_format($product->price, 2) }}</del>
+                                            @endif                                                  
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-xxl">
-                                <div class="fs-16">
-                                    <span class="d-block text-primary fw-600">$64.750</span>
-                                                                                            <del class="d-block opacity-70">$68.000</del>
-                                                                                    </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
-                                                                                                            <div class="col mb-2">
-                    <a href="https://demo.activeitzone.com/ecommerce/product/adidas-mens-originals-poly-tape-shorts-en9ka" class="d-block p-2 text-reset bg-white h-100 rounded">
-                        <div class="row gutters-5 align-items-center">
-                            <div class="col-xxl">
-                                <div class="img">
-                                    <img
-                                        class="lazyload img-fit h-140px h-lg-80px"
-                                        src="https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg"
-                                        data-src="https://demo.activeitzone.com/ecommerce/public/uploads/all/u1DDNkgW9HCKqNHOYURJY6dJaqwiSwBBQj8S0jLH.png"
-                                        alt="Adidas Men&#039;s Originals Poly Tape Shorts"
-                                        onerror="this.onerror=null;this.src='https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg';"
-                                    >
-                                </div>
-                            </div>
-                            <div class="col-xxl">
-                                <div class="fs-16">
-                                    <span class="d-block text-primary fw-600">$102.000</span>
-                                                                                    </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                                                                                                            <div class="col mb-2">
-                    <a href="https://demo.activeitzone.com/ecommerce/product/adidas-womens-cropped-slim-polyester-shorts-jvdi7" class="d-block p-2 text-reset bg-white h-100 rounded">
-                        <div class="row gutters-5 align-items-center">
-                            <div class="col-xxl">
-                                <div class="img">
-                                    <img
-                                        class="lazyload img-fit h-140px h-lg-80px"
-                                        src="https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg"
-                                        data-src="https://demo.activeitzone.com/ecommerce/public/uploads/all/8LmfSZEGCktshcYjefNylyAHRdFwPqkKz7hwVG9R.png"
-                                        alt="Nautica Voyage By Nautica For Men Eau De Toilette Spray"
-                                        onerror="this.onerror=null;this.src='https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg';"
-                                    >
-                                </div>
-                            </div>
-                            <div class="col-xxl">
-                                <div class="fs-16">
-                                    <span class="d-block text-primary fw-600">$65.000</span>
-                                                                                    </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                                                                                                            <div class="col mb-2">
-                    <a href="https://demo.activeitzone.com/ecommerce/product/nike-men-mercurial-superfly-7-elite-firm-ground-football-shoe-zlwgn" class="d-block p-2 text-reset bg-white h-100 rounded">
-                        <div class="row gutters-5 align-items-center">
-                            <div class="col-xxl">
-                                <div class="img">
-                                    <img
-                                        class="lazyload img-fit h-140px h-lg-80px"
-                                        src="https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg"
-                                        data-src="https://demo.activeitzone.com/ecommerce/public/uploads/all/LfCYkZUyEVnUSjqjznaA6MVuLETNLDkaTMnCud9F.png"
-                                        alt="Under Armour Men&#039;s Charged Assert 9 Running Shoe"
-                                        onerror="this.onerror=null;this.src='https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg';"
-                                    >
-                                </div>
-                            </div>
-                            <div class="col-xxl">
-                                <div class="fs-16">
-                                    <span class="d-block text-primary fw-600">$97.200</span>
-                                                                                            <del class="d-block opacity-70">$120.000</del>
-                                                                                    </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                                                                                                            <div class="col mb-2">
-                    <a href="https://demo.activeitzone.com/ecommerce/product/FIFA-22-Standard---PC-Online-Game-Code-22545" class="d-block p-2 text-reset bg-white h-100 rounded">
-                        <div class="row gutters-5 align-items-center">
-                            <div class="col-xxl">
-                                <div class="img">
-                                    <img
-                                        class="lazyload img-fit h-140px h-lg-80px"
-                                        src="https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg"
-                                        data-src="https://demo.activeitzone.com/ecommerce/public/uploads/all/4gmGnYIZhgatZRJsxrhXVW2qO0aoLLdZT8U6qOQE.png"
-                                        alt="FIFA 22 Standard - PC [Online Game Code]"
-                                        onerror="this.onerror=null;this.src='https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg';"
-                                    >
-                                </div>
-                            </div>
-                            <div class="col-xxl">
-                                <div class="fs-16">
-                                    <span class="d-block text-primary fw-600">$56.050</span>
-                                                                                            <del class="d-block opacity-70">$59.000</del>
-                                                                                    </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                                                                                                            <div class="col mb-2">
-                    <a href="https://demo.activeitzone.com/ecommerce/product/apple-iphone-14-pro-128gb-512gb-deep-purple-unlocked" class="d-block p-2 text-reset bg-white h-100 rounded">
-                        <div class="row gutters-5 align-items-center">
-                            <div class="col-xxl">
-                                <div class="img">
-                                    <img
-                                        class="lazyload img-fit h-140px h-lg-80px"
-                                        src="https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg"
-                                        data-src="https://demo.activeitzone.com/ecommerce/public/uploads/all/IaqgwBAe6tTXCfDwHIRham8S5rFXLC77elMybtZC.webp"
-                                        alt="Apple iPhone 14 Pro , 128GB, 512GB, Deep Purple- Unlocked"
-                                        onerror="this.onerror=null;this.src='https://demo.activeitzone.com/ecommerce/public/assets/img/placeholder.jpg';"
-                                    >
-                                </div>
-                            </div>
-                            <div class="col-xxl">
-                                <div class="fs-16">
-                                    <span class="d-block text-primary fw-600">$999.000</span>
-                                                                                    </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                                                                        </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
